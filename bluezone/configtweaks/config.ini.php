74de14906dac2c74ee89a512183fa01c52c72765
Sheng Hao Zhu <shhaozhu@cn.ibm.com>
2017-06-19 14:09:13 +0800

    Persisting the installer wizard generated config.ini.php

---
+++ bluezone/configtweaks/config.ini.php
@@ -0,0 +1,102 @@
+; <?php exit; ?> DO NOT REMOVE THIS LINE
+; file automatically generated or modified by Piwik; you can manually override the default values in global.ini.php by redefining them in this file.
+[database]
+host = "169.46.25.61"
+username = "cognitivehubadmin"
+password = "aq1sw2de"
+dbname = "piwik_db"
+tables_prefix = "piwik_"
+
+[Debug]
+enable_measure_piwik_usage_in_idsite = 0
+allow_upgrades_to_beta = 0
+
+[General]
+enabled_periods_UI = "day,week,month,year"
+enabled_periods_API = "day,week,month,year"
+action_category_level_limit = 3
+show_multisites_sparklines = 0
+anonymous_user_enable_use_segments_API = 0
+browser_archiving_disabled_enforce = 1
+enable_create_realtime_segments = 0
+enable_segment_suggested_values = 0
+adding_segment_requires_access = "superuser"
+allow_adding_segments_for_all_websites = 0
+datatable_row_limits = "5,10,25,50"
+enable_browser_archiving_triggering = 0
+force_ssl = 1
+multisites_refresh_after_seconds = 0
+assume_secure_protocol = 1
+enable_delete_old_data_settings_admin = 0
+enable_auto_update = 0
+proxy_client_headers[] = "HTTP_X_CLIENT_IP"
+proxy_client_headers[] = "HTTP_X_FORWARDED_FOR"
+proxy_client_headers[] = "HTTP_X_CLUSTER_CLIENT_IP"
+proxy_client_headers[] = "HTTP_CLIENT_IP"
+salt = "f5966e6d7129e4f40f6355e6b967e074"
+proxy_host_headers = "HTTP_X_FORWARDED_HOST"
+trusted_hosts[] = "cognitivehub-analytics.w3ibm.mybluemix.net"
+
+[Tracker]
+new_visit_api_requires_admin = 0
+
+[PluginsInstalled]
+PluginsInstalled[] = "Diagnostics"
+PluginsInstalled[] = "Login"
+PluginsInstalled[] = "CoreAdminHome"
+PluginsInstalled[] = "UsersManager"
+PluginsInstalled[] = "SitesManager"
+PluginsInstalled[] = "Installation"
+PluginsInstalled[] = "Monolog"
+PluginsInstalled[] = "Intl"
+PluginsInstalled[] = "CorePluginsAdmin"
+PluginsInstalled[] = "CoreHome"
+PluginsInstalled[] = "WebsiteMeasurable"
+PluginsInstalled[] = "CoreVisualizations"
+PluginsInstalled[] = "Proxy"
+PluginsInstalled[] = "API"
+PluginsInstalled[] = "ExamplePlugin"
+PluginsInstalled[] = "Widgetize"
+PluginsInstalled[] = "Transitions"
+PluginsInstalled[] = "LanguagesManager"
+PluginsInstalled[] = "Actions"
+PluginsInstalled[] = "Dashboard"
+PluginsInstalled[] = "MultiSites"
+PluginsInstalled[] = "Referrers"
+PluginsInstalled[] = "UserLanguage"
+PluginsInstalled[] = "DevicesDetection"
+PluginsInstalled[] = "Goals"
+PluginsInstalled[] = "Ecommerce"
+PluginsInstalled[] = "SEO"
+PluginsInstalled[] = "Events"
+PluginsInstalled[] = "UserCountry"
+PluginsInstalled[] = "VisitsSummary"
+PluginsInstalled[] = "VisitFrequency"
+PluginsInstalled[] = "VisitTime"
+PluginsInstalled[] = "VisitorInterest"
+PluginsInstalled[] = "ExampleAPI"
+PluginsInstalled[] = "RssWidget"
+PluginsInstalled[] = "Feedback"
+PluginsInstalled[] = "CoreUpdater"
+PluginsInstalled[] = "CoreConsole"
+PluginsInstalled[] = "ScheduledReports"
+PluginsInstalled[] = "UserCountryMap"
+PluginsInstalled[] = "Live"
+PluginsInstalled[] = "CustomVariables"
+PluginsInstalled[] = "PrivacyManager"
+PluginsInstalled[] = "ImageGraph"
+PluginsInstalled[] = "Annotations"
+PluginsInstalled[] = "MobileMessaging"
+PluginsInstalled[] = "Overlay"
+PluginsInstalled[] = "SegmentEditor"
+PluginsInstalled[] = "Insights"
+PluginsInstalled[] = "Morpheus"
+PluginsInstalled[] = "Contents"
+PluginsInstalled[] = "BulkTracking"
+PluginsInstalled[] = "Resolution"
+PluginsInstalled[] = "DevicePlugins"
+PluginsInstalled[] = "Heartbeat"
+PluginsInstalled[] = "Marketplace"
+PluginsInstalled[] = "ProfessionalServices"
+PluginsInstalled[] = "UserId"
+PluginsInstalled[] = "CustomPiwikJs"
